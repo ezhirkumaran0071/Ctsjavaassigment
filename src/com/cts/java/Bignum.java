12db4528f37856a6cc7ba249c22b9f68055424a2
ezhirkumaran0071 <barubarathan@gmail.com>
2018-05-22 15:44:08 +0530

    logic modified

--- src/com/cts/java/Bignum.java
+++ src/com/cts/java/Bignum.java
@@ -3,16 +3,16 @@ package com.cts.java;
 public class Bignum 
 {
 	
-	public String isbig(int num1,int num2)
+	public int big(int num1,int num2)
 	{
 		if(num1>num2)
 		{
-			return "x is greater";
+			return num1;
 			
 		}
 		else
 		{
-			return "y is greater";
+			return num2;
 		}
 	}
 
